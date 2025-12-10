e7a761e10669ae65bbf10b6013200e5576dd1249
Jeferson Pinzon <s4lpicong4mer3993@gmail.com>
2025-12-10 11:47:40 -0500

    secure

--- client/proxy-plugin/src/main/java/com/decloid/deCloidUtils/KeyMap.java
+++ client/proxy-plugin/src/main/java/com/decloid/deCloidUtils/KeyMap.java
@@ -10,8 +10,8 @@ public class KeyMap {
 
     static {
         // 🔥 Aquí defines tus claves
-        keyToUUID.put("skywars", UUID.fromString("c8a78d2d-db48-4799-96b4-f83a3e09e0ba"));
-        keyToUUID.put("prueba2", UUID.fromString("3fe4f0e0-45a8-4d0a-bfc9-1abcdaec2f88"));
+        keyToUUID.put("skywars", UUID.fromString("f4074b11-2de1-4005-b179-e7b7564364cb"));
+        keyToUUID.put("bedwars", UUID.fromString("dd4671c4-f404-4626-93a8-3ba226cea4a9"));
         keyToUUID.put("prueba3", UUID.fromString("a8dacc48-bf53-4df5-b3d7-1be6a1c34b90"));
     }
 
